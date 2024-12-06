e7e1b8ea2551a73f2e7c1938441a94601f39adb2
PerHac13 <abdullah13112004@gmail.com>
2024-12-06 09:05:51 +0530

    feat: initial configurations setup - Added configuration files:   - tsconfig.json: TypeScript configuration.   - eslintrc.js and eslintignore: ESLint configuration and ignore rules.   - prettierrc.json and prettierignore: Prettier configuration and ignore rules. - Included project metadata:   - package.json, README.md, LICENSE, and .gitignore.

---
+++ config/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    'no-console': 'warn',
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
+};
