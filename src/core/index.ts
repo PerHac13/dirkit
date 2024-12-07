976310faff630bb776c5546d229fc8c463b5d4d0
PerHac13 <abdullah13112004@gmail.com>
2024-12-07 13:01:54 +0530

    feat: export core classes from index module

---
+++ src/core/index.ts
@@ -0,0 +1,7 @@
+// Note: Export all core classes
+
+export { TreeGenerator, DirectoryTree, TreeGeneratorOptions } from './tree-generator';
+
+export { FileParser } from './file-parser';
+
+export { DirectoryComparator } from './comparator';
